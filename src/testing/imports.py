2b05fa263bf1bec1e00734939f804434114573d8
Julian Ding <scorpiusa51@gmail.com>
2021-04-05 14:27:27 -0700

    Add basic test to make sure imports work

---
+++ src/testing/imports.py
@@ -0,0 +1,15 @@
+# Testing imports
+
+import logging
+
+import utils
+
+PACKAGES = [utils]
+
+def list_all_modules():
+    for pkg in PACKAGES:
+        all_names = dir(pkg)
+        public = [n for n in all_names if not n.startswith('_')]
+        private = [n for n in all_names if n.startswith('_')]
+        logging.debug(f"Imported package {getattr(pkg, '__name__')}\n"
+                    + f"Public attributes: {public}\nPrivate attributes: {private}")
